719a6b77f699aea668ff1d4e6265618b3f39019e
HENRY Christophe <christophe.henry2@soprabanking.com>
2023-06-13 19:28:03 +0200

    implémentation du pendu en console

---
+++ src/com/dyma/game/GuessGame.java
@@ -0,0 +1,63 @@
+package com.dyma.game;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * TODO
+ */
+public class GuessGame {
+
+    /**
+     * Stores the secret word that the player wants to discover
+     */
+    private final List<Character> secretWord = new ArrayList<>();
+    private int lifePoints;
+    private final List<Character> guessWord = new ArrayList<>();
+    private final Set<Character> guessedLetters = new HashSet<>();
+
+
+    public GuessGame(String wordToGuess, int lifePoints) {
+        for (char c : wordToGuess.toCharArray()) {
+            this.secretWord.add(c);
+        }
+        this.lifePoints = lifePoints;
+        for (int index = 0; index < this.secretWord.size(); index++) {
+            this.guessWord.add('_');
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "GuessGame{" +
+                "lifePoints=" + lifePoints +
+                ", guessWord=" + guessWord +
+                ", guessedLetters=" + guessedLetters +
+                '}';
+    }
+
+    public void guessLetter(char letter) {
+        guessedLetters.add(letter);
+        if (secretWord.contains(letter) && !guessWord.contains(letter)) {
+            var index = 0;
+            for (char c : secretWord) {
+                if (c == letter) {
+                    guessWord.set(index, c);
+                }
+                index ++;
+            }
+        } else {
+            lifePoints -= 1;
+        }
+    }
+
+    public boolean isLost() {
+        return lifePoints <= 0;
+    }
+
+    public boolean isWon() {
+        return !guessWord.contains('_');
+    }
+}
